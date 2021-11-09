590a012f825d23e466c80a90c6e1fa93d867aeeb
michal <michal.wilk0@yahoo.com>
2021-11-09 01:22:32 +0100

    Updated backup script files

--- install.sh
+++ install.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-echo "Configuring the machine" | tee log.txt
-
-# first repo containing files to backup
-git init generated_files
-
 # second very important repo for cracking enigma codes and curing cancer
 git clone https://github.com/kevva/is-positive.git
